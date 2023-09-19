08ec7bc09d1e1a221fa7a38df3633f5058b584c9
Chris obrien <chrisobrien88@gmail.com>
2023-09-19 21:05:01 +0200

    very simple component for jira board

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, { render, Fragment, Text, IssuePanel, Button } from '@forge/ui';
 
 const App = () => {
   return (
     <Fragment>
+      <Button text="Click me" onClick={() => console.log('Button clicked!')} />
       <Text>Hello world!</Text>
+      
     </Fragment>
   );
 };
