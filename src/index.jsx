a9d9309f9df4a235d0af0a091ac58cc7a71eca62
Chris obrien <chrisobrien88@gmail.com>
2023-09-25 10:25:52 +0200

    simple counter experiment finished

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,37 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel, Button } from '@forge/ui';
+import ForgeUI, {
+  render,
+  Fragment,
+  Text,
+  Button,
+  IssuePanel,
+  useProductContext,
+  useState,
+} from "@forge/ui";
+import api, { route } from "@forge/api";
+
+const fetchCommentsForIssue = async (issueIdOrKey) => {
+  const res = await api
+    .asUser()
+    .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`);
+  const data = await res.json();
+  return data.comments;
+};
 
 const App = () => {
+  const context = useProductContext();
+  const [count, setCount] = useState(0);
+
+  const [comments] = useState(
+    async () => await fetchCommentsForIssue(context.platformContext.issueKey)
+  );
+
+  console.log(`Number of comments on this issue: ${comments.length}`);
   return (
     <Fragment>
-      <Button text="Click me" onClick={() => console.log('Button clicked!')} />
-      <Text>Hello world!</Text>
-      
+      <Text>Hello there</Text>
+      <Text>Number of comments on this issue: {comments.length}</Text>
+      <Text>Number of times clicked: {count}</Text>
+      <Button text="click me please" onClick={()=> {setCount(count + 1); console.log("clicked: ", count, " times")}}></Button>
     </Fragment>
   );
 };
